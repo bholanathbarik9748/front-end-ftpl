110044cbd0bf62bcf8f5e5dccb817fd825084be5
Bholanath <bholanathbarik9748@gmail.com>
2025-02-11 15:41:25 +0530

    Feat: Create routes for login and singup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Home from '@/pages/Home'
+import Home from '@/page/Home'
 import React from 'react'
 
 const page = () => {
---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import SignUp from "@/page/SignUp";
+import React from "react";
+
+function page() {
+  return <SignUp />;
+}
+
+export default page;
